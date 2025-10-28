9c84c898de59afc20178d7e0fc895ae42399715a
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-10-28 22:38:38 +0530

    Create 03.environment.sv

---
+++ UVM_VERIFICATION/AND_GATE/01.AND_gate/03.environment.sv
@@ -0,0 +1,26 @@
+class environment extends uvm_env;
+  `uvm_component_utils(environment)
+  
+  agent agnt;
+  scoreboard scb;
+  
+  function new(string name = "environment",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    
+    agnt = agent::type_id::create("agnt",this);
+    scb  = scoreboard :: type_id::create("scb",this);
+    
+    `uvm_info(get_type_name(),"build-phase from environment",UVM_NONE)
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    agnt.mon.port_tx.connect(scb.port_rx);
+    `uvm_info(get_type_name(),"connect-phase from environment",UVM_NONE)
+  endfunction
+
+endclass
