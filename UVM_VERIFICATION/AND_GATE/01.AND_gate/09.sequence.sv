90b1c79ea6b96184f39f5ebf225f0591d22bafa7
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-10-28 22:43:00 +0530

    Create 09.sequence.sv

---
+++ UVM_VERIFICATION/AND_GATE/01.AND_gate/09.sequence.sv
@@ -0,0 +1,27 @@
+class seq extends uvm_sequence#(sequence_item);
+  
+  `uvm_object_utils(seq)
+  
+  sequence_item tx;
+  
+  function new(string name = "seq");
+    super.new(name);
+  endfunction
+  
+  //no-phases
+  
+  task body();
+    repeat(20) begin
+     `uvm_info(get_type_name(),"---------body started------",UVM_NONE) 
+     tx = sequence_item::type_id::create("tx");
+// 	 assert(tx.randomize());
+     `uvm_do(tx);
+      
+//       start_item(tx);
+//       assert(tx.randomize());
+//       finish_item(tx);
+   end
+    
+  endtask
+    
+endclass
