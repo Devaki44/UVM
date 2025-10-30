45705b833df8c37366ee351e8f715708c4c588c6
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-10-30 22:52:13 +0530

    Create 10.sequence_item.sv

---
+++ UVM_VERIFICATION/FULL_ADDER/10.sequence_item.sv
@@ -0,0 +1,22 @@
+class sequence_item extends uvm_sequence_item;
+  
+  rand logic a,b,cin;
+  	   logic sum,cout;
+  
+  `uvm_object_utils_begin(sequence_item)
+  `uvm_field_int(a,UVM_ALL_ON)
+  `uvm_field_int(b,UVM_ALL_ON)
+  `uvm_field_int(cin,UVM_ALL_ON)
+  `uvm_field_int(sum,UVM_ALL_ON)
+  `uvm_field_int(cout,UVM_ALL_ON)
+  `uvm_object_utils_end
+
+  function new(string name = "sequence_item");
+    super.new(name);
+    `uvm_info("SEQ_ITEM","constructor",UVM_HIGH)
+  endfunction
+  
+  
+endclass
+  
+  
