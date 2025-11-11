4ed1b7270a54a34b602ecea1af76da3b6bad7a36
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-11-11 22:22:17 +0530

    Create 02.TEST.sv

---
+++ CONCEPTS/GLOBAL_OVERRIDDING/02.TEST.sv
@@ -0,0 +1,45 @@
+class test extends uvm_test;
+  `uvm_component_utils(test)
+  
+  environment env;
+  seq se;
+  
+  function new(string name = "test",uvm_component parent);
+    super.new(name,parent);
+    `uvm_info("TEST_CLASS","constructor",UVM_HIGH)
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    
+    env = environment :: type_id :: create("env",this);
+    se  = seq		  :: type_id :: create("se",this);
+    
+    set_type_override_by_type(agent1::get_type(),agent2::get_type());
+    
+    `uvm_info("TEST_CLASS","build_phase from test",UVM_LOW)
+  endfunction
+  
+  function void end_of_elaboration_phase(uvm_phase phase);
+    super.end_of_elaboration_phase(phase);
+    
+    `uvm_info("TEST_CLASS","end_of_elaboration_phase from test",UVM_LOW)
+    print();
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    
+    phase.raise_objection(this);
+    se.start(env.A1.seqr);
+    phase.drop_objection(this);
+    `uvm_info("TEST_CLASS","run_phase from test",UVM_LOW)
+    `uvm_info("TEST_CLASS","------------------------",UVM_LOW)
+
+
+  endtask
+  
+endclass
+    
+  
+  
