28a5de029c83a2ebba936516674b7b097ad67ef7
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2026-01-20 22:23:43 +0530

    Create 05.AGENT.sv

---
+++ UVM_VERIFICATION/ASYNC_FIFO/ASYNC_FIFO/uvm_files/05.AGENT.sv
@@ -0,0 +1,29 @@
+class agent extends uvm_agent;
+  `uvm_component_utils(agent)
+  
+  sequencer seqr;
+  driver dri;
+  monitor mon;
+  
+  function new(string name = "agent",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    `uvm_info(get_type_name(),"build_phase from agent",UVM_HIGH)
+    
+    seqr = sequencer :: type_id :: create("seqr",this);
+    dri  = driver    :: type_id :: create("dri",this);
+    mon  = monitor   :: type_id :: create("mon",this);
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    `uvm_info(get_type_name(),"connect_phase from agent",UVM_HIGH)
+    
+    dri.seq_item_port.connect(seqr.seq_item_export);
+  endfunction
+  
+endclass
+    
